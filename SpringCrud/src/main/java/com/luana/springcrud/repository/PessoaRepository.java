4367caaa635cc88976381ed63e733137ae04867f
Luana Machado Hilbert <110848840+LuanaHilbert@users.noreply.github.com>
2025-02-27 20:28:48 -0300

    Delete SpringCrud directory

--- SpringCrud/src/main/java/com/luana/springcrud/repository/PessoaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.luana.springcrud.repository;
-
-import com.luana.springcrud.model.Pessoa;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PessoaRepository extends JpaRepository<Pessoa, Integer> {
-}
