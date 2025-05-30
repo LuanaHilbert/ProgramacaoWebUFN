0bf8fdb6005867e16b4f394f6aa1cb681751ab59
luana <luanahilbert@gmail.com>
2025-05-29 21:56:30 -0300

    aula

--- menu_backend/src/main/java/com/example/menu_backend/repository/ProdutoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.menu_backend.repository;
-
-import com.example.menu_backend.model.Produto;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProdutoRepository extends JpaRepository<Produto, Long> {
-}
